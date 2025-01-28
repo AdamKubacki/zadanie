8dba5bf5c6c2298e6927d127985acd00c01d9845
AdamKubacki <adamkubacki@mat.umk.pl>
2025-01-28 23:34:15 +0100

    Program sprawdza czy liczba jest ujmna

--- program.cpp
+++ program.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
 
+unsigned long long silnia(int n) {
+    if (n == 0 || n == 1) return 1;
+    return n * silnia(n - 1);
+}
+
+bool cz_ujemna(int n)
+{
+    if(n < 0)
+    {
+        return true;
+    }
+    else
+    {
+    return false;
+    }
+}
+
 int main() {
     int number;
     cin >> number;
+    if(cz_ujemna(number))
+    {
+        cout << "Liczba Ujemna" << endl;
+        return 0;
+    }
     cout << number << endl;
     return 0;
 }
