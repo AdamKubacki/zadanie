b26be3caf4f40c4eb3435e3eec656b09d7213363
AdamKubacki <adamkubacki@mat.umk.pl>
2025-01-28 23:18:56 +0100

    Program liczy silnie

--- program.cpp
+++ program.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
+unsigned long long silnia(int n) {
+    if (n == 0 || n == 1) return 1;
+    return n * silnia(n - 1);
+}
+
 int main() {
     int number;
     cin >> number;
-    cout << number << endl;
+    cout << silnia(number) << endl;
     return 0;
 }
